0b0b29bd7835ffddae54ea5acaae060700b680db
Ana Carolina <anacarolinagarciafreitas@gmail.com>
2024-10-10 10:40:52 -0300

    feat: create the post route

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -33,6 +33,37 @@ suspeitosRoutes.get("/", (req, res) => {
     return res.status(200).json(suspeitos);
   });
 
+  // Rota para cadastrar um novo suspeito
+suspeitosRoutes.post("/", (req, res) => {
+    const { nome, idade, descricaoFisica, atividadesSuspeitas } = req.body;
+
+    console.log(Number.isInteger(idade));
   
+    // Validação dos campos obrigatórios
+    if (!nome || !idade || !atividadesSuspeitas) {
+      return res.status(400).json({
+        message: "Os campos nome, idade e atividadesSuspeitas são obrigatórios!",
+      });
+    }
+  
+    // Validação de atividadesSuspeitas
+    if (atividadesSuspeitas != "sim" && atividadesSuspeitas != "não") {
+      return res.status(400).send({
+        message: "Digite 'sim' ou 'não'!",
+      });
+    }
+    const novoSuspeito = {
+        id: Number(Math.floor(Math.random() * 99) + 1),
+        nome,
+        idade,
+        descricaoFisica,
+        atividadesSuspeitas
+      };
+    
+      suspeitos.push(novoSuspeito);
+    
+      return res.status(201).send({message: "Novo suspeito cadastrado", novoSuspeito});
+})
+
 
   export default suspeitosRoutes;
